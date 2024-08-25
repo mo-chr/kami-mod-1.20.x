204b95300ecbd12add1e5b23f685ffd4430424ba
Mohammad Chreif <cmohamadch@gmail.com>
2024-08-26 02:38:33 +0300

    Base project for mods

---
+++ src/main/java/com/kamikode/kamimod/KamiModClient.java
@@ -0,0 +1,9 @@
+package com.kamikode.kamimod;
+import net.fabricmc.api.ClientModInitializer;
+
+public class KamiModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
